81bac31634c126b14241cd72b6a88b9a95c6ba32
Marifei <116741086+Marina-Bordug@users.noreply.github.com>
2024-04-16 17:37:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 // d.innerHTML = list[0]["text"]
 
-document.querySelector("button").addEventListener("click", (e)=> {
-    
+document.querySelector("button").addEventListener("click", (e) => {
+
     document.querySelector(".topics").classList.toggle("hidden");
     document.querySelector(".active-b").classList.toggle("passive-b");
 })
@@ -13,12 +13,14 @@ document.querySelector("button").addEventListener("click", (e)=> {
 let content = document.querySelector(".topic-content")
 let links = document.querySelectorAll(".color-change")
 links.forEach(
-    (link)=>{
-        
+    (link) => {
+
         link.addEventListener("click", event => {
+
+            link.style.color = "#b44f34"
             content.innerHTML = list[link.dataset.id]["text"]
             let st = window.getComputedStyle(content, null);
-            let bottom = -parseInt(st['height'])/6;
+            let bottom = -parseInt(st['height']) / 6;
 
             let startBottom = bottom;
             let w = 0;
@@ -28,17 +30,17 @@ links.forEach(
             let timerId = setInterval(() => {
                 content.style.opacity = `${op}`;
                 content.style.bottom = `${bottom}px`;
-                bottom += Math.abs(startBottom)/100;
-                w+=1;
-                op+=1/100;
-                if (w>100) {
+                bottom += Math.abs(startBottom) / 100;
+                w += 1;
+                op += 1 / 100;
+                if (w > 100) {
                     content.style.position = 'static';
                     clearInterval(timerId);
-                    
+
                 }
             }, 3);
-            
-            
+
+
         });
     }
 )
