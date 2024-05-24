be155ef67e90736fbb87d4c6ca5cc072aab34e45
Marifei <116741086+Marina-Bordug@users.noreply.github.com>
2024-05-24 16:10:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // d.innerHTML = list[0]["text"]
 const topics = document.querySelector(".topics")
-const topicContent =  document.querySelector(".topic-content")
+const topicContent = document.querySelector(".topic-content")
 document.querySelector(".active-b").addEventListener("click", (e) => {
     document.querySelector(".topics").classList.toggle("hidden");
     // document.querySelector(".topics").classList.toggle("nonhidden");
@@ -17,42 +17,95 @@ document.querySelector(".active-b").addEventListener("click", (e) => {
     }
 
     let timerId = setInterval(() => {
-        
+
         if (topics.classList.contains("hidden")) {
-            startLeft-=5
+            startLeft -= 5
         } else {
-            startLeft+=5
+            startLeft += 5
         }
         topics.style.left = `${startLeft}px`;
-        topicContent.style.paddingLeft = `${300+startLeft}px`;
-        console.log(startLeft)
-        if (startLeft>=0 || startLeft <= -300) {
+        topicContent.style.paddingLeft = `${300 + startLeft}px`;
+        // console.log(startLeft)
+        if (startLeft >= 0 || startLeft <= -300) {
             clearInterval(timerId);
 
         }
-        
+
     }, 1);
-     
-    
-    
+
+
+
 })
 
 let content = document.querySelector(".topic-content")
 let links = document.querySelectorAll(".color-change")
 links[0].classList.add("active-link");
-content.innerHTML = list[links[0].dataset.id]["text"];
+
+// let c = 1
+
+for (i = 0; i < links.length; i ++) {
+   
+}
+
+
+
+
+
+
 links.forEach(
     (link) => {
 
         link.addEventListener("click", event => {
             links.forEach(
                 (linka) => {
-                    linka.classList.remove("active-link"); 
+                    linka.classList.remove("active-link");
                 }
             )
             link.classList.add("active-link");
             // link.style.backgroundColor = "#b44f34";
-            content.innerHTML = list[link.dataset.id]["text"];
+
+
+
+    
+            let divs = list[link.dataset.id]["text"];
+            let codes = divs.split('coder').length - 1;
+            let codeList = divs.split('coder');
+            let finalCode = codeList[0]
+            let qqq = []
+            
+            for (let index = 1; index <= codes; index++) {
+                qqq.push('coder' + index)
+                finalCode += 'coder' + index + codeList[index]
+            }
+            // c = codes + 1
+            var code;
+            content.innerHTML = finalCode;
+            qqq.forEach((el) => {
+                code = ace.edit(el);
+                // console.log(code)                    // создаем редактор из элемента с id="code"
+                code.setTheme("ace/theme/twilight");           // выбираем тему оформления для подсветки синтаксиса
+                code.getSession().setMode("ace/mode/c_cpp");  // говорим что код надо подсвечивать как C++ код
+                code.setShowPrintMargin(false);               // опционально: убираем вертикальную границу в 80 сиволов
+                code.setOptions({
+                    // maxLines: Infinity,                    // опционально: масштабировать редактор вертикально по размеру кода
+                    fontSize: "12pt",
+                    readOnly: false,
+                    // опционально: размер шрифта ставим побольше
+                });
+                code.$blockScrolling = Infinity;              // отключаем устаревшие, не поддерживаемые фишки редактора
+            })
+
+
+
+
+
+
+
+
+
+
+
+            // content.innerHTML = list[link.dataset.id]["text"];
             let st = window.getComputedStyle(content, null);
             let bottom = -parseInt(st['height']) / 6;
 
@@ -78,6 +131,7 @@ links.forEach(
         });
     }
 )
+
 
 
 
